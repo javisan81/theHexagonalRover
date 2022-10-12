1ba3e9a0525173cbc8d4ce8803982b94cce5ccff
Javier Lopez Fernandez <javierlopezfernandez@MacBook-Pro-de-Javier.local>
2022-10-12 08:31:48 +0200

    bare springboot app running

---
+++ src/it/java/org.bcnjug/MarsRoverSmokeIT.java
@@ -0,0 +1,22 @@
+package org.bcnjug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MarsRoverSmokeIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void smokeTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+}
