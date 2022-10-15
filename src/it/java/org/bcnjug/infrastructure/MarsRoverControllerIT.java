9d40ea1dcdb0f3521e5ad4b3f55ec31d39a61987
Javier Lopez Fernandez <javierlopezfernandez@MacBook-Pro-de-Javier.local>
2022-10-15 08:20:08 +0200

    rename integration test

--- src/it/java/org/bcnjug/infrastructure/MarsRoverControllerIT.java
+++ src/it/java/org/bcnjug/infrastructure/MarsRoverControllerIT.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class MarsRoverIT {
+public class MarsRoverControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
